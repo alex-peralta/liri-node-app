700f2765b0b83e0f02fc7f3f3b090ef8a28fce15
Alex Peralta <aperalta@salesforce.com>
2018-04-21 20:54:35 -0400

    Setup of switch statement to handle parameter options

--- liri.js
+++ liri.js
@@ -1,12 +1,44 @@
+// use .env to hide keys
 require('dotenv').config();
 
 // Importing the API keys files from keys.js file
 const keys = require("./keys.js");
+const fs = require("fs");
+const request = require("request");
 
-// Creating new objects from imported file
-const spotify = new Spotify(keys.spotify);
+// Importing the API keys from keys.js file
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
 const client = new Twitter(keys.twitter);
+const spotify = new Spotify(keys.spotify);
 
-console.log(spotify.id);
-console.log(client);
-console.log(process.env.TWITTER_CONSUMER_KEY);
+// Storing of the command line options
+
+const appType = process.argv[2];
+const appOption = process.argv[3];
+
+switch (appType) {
+    case "my-tweets":
+        myTweets();
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+
+    default: console.log('ERROR: "No Parameters provided"' + '\n' +
+        'Options:' + '\n' +
+        '1.) node liri.js my-tweets' + '\n' +
+        '2.) node liri.js spotify-this-song "Use quotes for multi-word Titles"' + '\n' +
+        '3.) node liri.js movie-this' + '\n' +
+        '4.) node liri.js do-what-it-says' + '\n');
+
+}
